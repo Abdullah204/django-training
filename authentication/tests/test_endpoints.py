4bc89937ff402a40787c5e9cb630cd1c4fe91ea1
Abdullah Ahmad Fouad <abdullahahmadfouad@gmail.com>
2022-10-29 16:14:14 +0200

    login and register tests

--- authentication/tests/test_endpoints.py
+++ authentication/tests/test_endpoints.py
@@ -7,7 +7,8 @@ from rest_framework.test import APIClient
 import sys, os
 from users.models import User
 from django.contrib.auth import get_user_model
-
+from rest_framework.exceptions import ErrorDetail
+from rest_framework.response import Response
 
 
 @pytest.mark.django_db
@@ -44,6 +45,144 @@ def test_unAuthenticatedRegisterViewWithValidData():
     "password" : "adwmfqwpfqmpfqwf",
     "confirm_password" : "adwmfqwpfqmpfqwf"
     }
+    
     response = client.post('http://localhost:8000/authentication/register/',register_data)
     assert response.status_code == 200
 
+
+def test_RegisterUsernameRequired():
+    client = APIClient()
+
+    register_data = {
+    "email" : "activedadsadde@gmail.com",
+    "password" : "adwmfqwpfqmpfqwf",
+    "confirm_password" : "adwmfqwpfqmpfqwf"
+    }
+    response = client.post('http://localhost:8000/authentication/register/',register_data)
+    assert response.data == {'username': [ErrorDetail(string='This field is required.', code='required')]}
+
+def test_RegisterPasswordRequired():
+    client = APIClient()
+
+    register_data = {
+    "email" : "activedade@gmail.com",
+    "username" : "adwmfqwpfqmpfqwf",
+    "confirm_password" : "adwmfqwpfqmpfqwf"
+    }
+    response = client.post('http://localhost:8000/authentication/register/',register_data)
+    assert response.data == {'password': [ErrorDetail(string='This field is required.', code='required')]}
+
+
+def test_RegisterConfirm_PasswordRequired():
+    client = APIClient()
+
+    register_data = {
+    "email" : "activedade@gmail.com",
+    "username" : "adwmfqwpfqmpfqwf",
+    "password" : "adwmfqwpfqmpfqwf"
+    }
+    response = client.post('http://localhost:8000/authentication/register/',register_data)
+    assert response.data == {'confirm_password': [ErrorDetail(string='This field is required.', code='required')]}
+
+def test_RegisterConfirmEmailRequired():
+    client = APIClient()
+
+    register_data = {
+    "confirm_password" : "adwmfqwpfqmpfqwf",
+    "username" : "adqwdqdqwdqq",
+    "password" : "adwmfqwpfqmpfqwf"
+    }
+    response = client.post('http://localhost:8000/authentication/register/',register_data)
+    assert response.data == {'email': [ErrorDetail(string='This field is required.', code='required')]}
+
+
+@pytest.mark.django_db
+def test_adminRegisterView(auth_client):
+    user = User.objects.create(username = "authenticated" , password = "adqwifqifmq" 
+    , email = "adwada@gmail.com" )
+    user.is_superuser = True
+    client = auth_client(user)
+    register_data = {
+    "username" : "MegaCoredadp",
+    "email" : "activedade@gmail.com",
+    "password" : "adwmfqwpfqmpfqwf",
+    "confirm_password" : "adwmfqwpfqmpfqwf"
+    }
+    response = client.post('http://localhost:8000/authentication/register/',register_data)
+    assert response.status_code  == 200
+
+
+@pytest.mark.django_db
+def test_authenticatedLoginView(auth_client):
+    client = APIClient()
+
+    register_data = {
+    "username" : "MegaCoredadp",
+    "email" : "activedade@gmail.com",
+    "password" : "adwmfqwpfqmpfqwf",
+    "confirm_password" : "adwmfqwpfqmpfqwf"
+    }
+    login_data = {
+    "username" : "MegaCoredadp",
+    "password" : "adwmfqwpfqmpfqwf",
+    }
+    client.post('http://localhost:8000/authentication/register/',register_data) 
+    response = client.post('http://localhost:8000/authentication/login/',login_data) 
+    assert response.status_code  == 200
+
+
+
+@pytest.mark.django_db
+def test_authenticatedLoginViewIncorrectPassword(auth_client):
+    client = APIClient()
+
+    register_data = {
+    "username" : "MegaCoredadp",
+    "email" : "activedade@gmail.com",
+    "password" : "adwmfqwpfqmpfqwf",
+    "confirm_password" : "adwmfqwpfqmpfqwf"
+    }
+    login_data = {
+    "username" : "MegaCoredadp",
+    "password" : "adwmfqwpfqmpf",
+    }
+    client.post('http://localhost:8000/authentication/register/',register_data) 
+    response = client.post('http://localhost:8000/authentication/login/',login_data) 
+    assert response.status_code  == 400
+
+@pytest.mark.django_db
+def test_authenticatedLoginViewIncorrectUsername(auth_client):
+    client = APIClient()
+
+    register_data = {
+    "username" : "MegaCoredadp",
+    "email" : "activedade@gmail.com",
+    "password" : "adwmfqwpfqmpfqwf",
+    "confirm_password" : "adwmfqwpfqmpfqwf"
+    }
+    login_data = {
+    "username" : "MegaCored",
+    "password" : "adwmfqwpfqmpf",
+    }
+    client.post('http://localhost:8000/authentication/register/',register_data) 
+    response = client.post('http://localhost:8000/authentication/login/',login_data) 
+    assert response.status_code  == 400
+
+
+@pytest.mark.django_db
+def test_authenticatedLoginData(auth_client):
+    client = APIClient()
+
+    register_data = {
+    "username" : "MegaCoredadp",
+    "email" : "activedade@gmail.com",
+    "password" : "adwmfqwpfqmpfqwf",
+    "confirm_password" : "adwmfqwpfqmpfqwf"
+    }
+    login_data = {
+    "username" : "MegaCoredadp",
+    "password" : "adwmfqwpfqmpfqwf",
+    }
+    client.post('http://localhost:8000/authentication/register/',register_data) 
+    response = client.post('http://localhost:8000/authentication/login/',login_data) 
+    assert response.data ["username"] == "MegaCoredadp" and response.data ["bio"] == '' and response.data ["email"] == "activedade@gmail.com"  and response.data ["token"] 
